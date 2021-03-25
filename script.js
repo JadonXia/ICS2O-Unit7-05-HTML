a5438627eadad886dc58c5882cedd9a5bb670ce5
JadonXia <jadon.xia@ocsbstudent.ca>
2021-03-25 13:32:15 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// These are the variables I need to use for the program.
+let age = 0
+let day = 0
+
+// Next, I have to connect my JavaScript to the button in HTML. It will link to a function I will write out later.
+document.getElementById('button1').addEventListener('click', time)
+
+// Now, I have to write out what function 'time' does.
+function time () {
+  age = document.getElementById('text1').value
+  day = document.getElementById('text2').value
+  age = parseInt(age)
+  if ((age <= 18) && (day != 'Saturday' && day != 'Sunday')) {
+    document.getElementById('results').innerHTML = 'It\'s time to go to school!'
+  } else if ((age > 18) && (day != 'Saturday' && day != 'Sunday')) {
+    document.getElementById('results').innerHTML = 'It\'s time to go to work!'
+  } else {
+    document.getElementById('results').innerHTML = 'It\'s finally time to relax for the weekend!'
+  }
+}
